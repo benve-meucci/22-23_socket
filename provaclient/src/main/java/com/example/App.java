0f9ed28b18012e281bb342083d5c6d1059239b59
Claudio Benvenuti <claudio@benve.it>
2022-10-18 09:09:46 +0200

    esercizio base

--- provaclient/src/main/java/com/example/App.java
+++ provaclient/src/main/java/com/example/App.java
@@ -1,28 +1,23 @@
 package com.example;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
+public class App {
+    public static void main(String[] args) throws Exception {
         Socket s = new Socket("localhost", 3000);
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-
+        // buffer input dal socket
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        //buffer input da tastiera
         BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Inserisci il messaggio:");
         String stringaUtente = tastiera.readLine();
-        
-        pr.println(stringaUtente);
-        pr.flush();
-
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader br = new BufferedReader(in);
-
-        String str = br.readLine();
-        System.out.println("Server: " + str);
+        out.writeBytes(stringaUtente + "\n");
+        String str = in.readLine();
+        System.out.println("Server ha inviato: " + str);
         s.close();
     }
 }
--- provaserver/src/main/java/com/example/App.java
+++ provaserver/src/main/java/com/example/App.java
@@ -1,27 +1,19 @@
 package com.example;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
+public class App {
+    public static void main(String[] args) throws Exception {
         ServerSocket ss = new ServerSocket(3000);
-        System.out.println("Server in ascolto sulla porta 3000");
         Socket s = ss.accept();
-        System.out.println("Client connesso");
-
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader br = new BufferedReader(in);
-        String str = br.readLine();
-        System.out.println("Client: " + str);
-
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Ben arrivato");
-        pr.flush();
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        String str = in.readLine();
+        out.writeBytes(str.toUpperCase() + "\n");
         s.close();
         ss.close();
 
